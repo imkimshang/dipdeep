0a43d547d208818c01ab6b297c1eef1c7cfd40a0
sooyoung <imkimshang@gmail.com>
2025-12-31 18:36:05 +0900

    feat:Suspense import 추가

--- app/workbook/week7/page.tsx
+++ app/workbook/week7/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect, useMemo } from 'react'
+import { useState, useEffect, useMemo, Suspense } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import {
   Folder,
